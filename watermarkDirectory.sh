56ef769c10fd5822b159b3176799bf7d85dee155
Alan Jackoway <alan.jackoway@gmail.com>
2018-06-22 11:30:59 -0400

    Updated to dynamically sized single watermark
    Get rid of the tiling and replace with a larger, dynamically-sized text watermark.

--- watermarkDirectory.sh
+++ watermarkDirectory.sh
@@ -3,14 +3,8 @@
 # and a custom text all over
 # Tested with ImageMagick 7 on Windows with Cygwin
 COPYRIGHT_TEXT="Gonzoway Photography"
-FONT="Arial"
-FILENAME_FONT_SIZE=400
-COPYRIGHT_FONT_SIZE=72
-
-COPYRIGHT_INTERMEDIATE_FILE=copyright_tile.png
-
-# Build the copyright file
-magick -font $FONT -pointsize $COPYRIGHT_FONT_SIZE -size 900x400 xc:none -fill grey -gravity Center -draw "text 0,0 '$COPYRIGHT_TEXT'" $COPYRIGHT_INTERMEDIATE_FILE
+FONT="Segoe-Print"
+FILENAME_FONT_SIZE=200
 
 # Get the directory name
 DIR_NAME=$(basename $PWD)
@@ -20,10 +14,18 @@ i=1
 # Iterate over all jpgs
 ls *.JPG | while read image
 do
-  echo "Handling $image as $DIR_NAME-$i"
-  # Tile copyright
-  magick composite -tile $COPYRIGHT_INTERMEDIATE_FILE $image wmark_$image
-  # Tag corner
-  magick wmark_$image -font $FONT -pointsize $FILENAME_FONT_SIZE -draw "gravity southeast fill black text 0, 0 '$DIR_NAME-$i'" wmark_$image
+  # Get the width of the image for copyright text
+  # Add copyright text
+  SIZE=$(magick identify $image | cut -d ' ' -f 3)
+  echo "Handling $image as $DIR_NAME-$i size $SIZE" 
+  # Experimentally found that image size / 10 - 100 was about right
+  DYNAMIC_SIZE="${SIZE:0:2}0"
+  DYNAMIC_SIZE=$((DYNAMIC_SIZE-100))
+  echo "Font size $DYNAMIC_SIZE"
+  magick $image -font "$FONT" -pointsize $DYNAMIC_SIZE -draw "gravity center fill #A0A0A0A0 text 0, 0 '$COPYRIGHT_TEXT'" wmark_$image
+
+  # Tag bottom with directory name + number
+  magick wmark_$image -font "$FONT" -pointsize $FILENAME_FONT_SIZE -draw "gravity south fill black text 0, 0 '$DIR_NAME-$i'" wmark_$image
+  magick wmark_$image -font "$FONT" -pointsize $FILENAME_FONT_SIZE -draw "gravity south fill white text 10, 10 '$DIR_NAME-$i'" wmark_$image
   i=$((i+1))
 done
